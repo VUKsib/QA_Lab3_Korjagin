0c5daa5f93e86ef5e640da8462098bb74c367ed4
VUKsib <korjaginsv22@mail.ru>
2023-04-23 00:21:43 +0700

    QA_Lab3_Korjagin
    QA_Lab3_Korjagin

---
+++ QA_Lab3/Flat.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QA_Lab3
+{
+    /// <summary>
+    /// Базовый класс "Комната"
+    /// <details>Данный класс нужен для хранения и обработки информации об одной комнате</details>
+    /// </summary>
+    internal class Room
+    {
+        protected double area; // площадь
+        protected int amount; // кол - во проживающих
+        /// <summary>
+        /// Конструктор класса Room, инициализирующий переменные
+        /// </summary>
+        /// <param name="s">площадь</param>
+        /// <param name="a">количество проживающих</param>
+        public void Init(double s, int a)
+        {
+            area = s;
+            amount = a;
+        }
+        /// <summary>
+        /// Отображает данные о комнате на экране
+        /// </summary>
+        public void Display()
+        {
+            Console.WriteLine("Площадь комнаты: " + area + "\nКоличество проживающих людей: " + amount + "\n");
+        }
+        /// <summary>
+        /// Считает площадь на человека
+        /// </summary>
+        /// <returns></returns>
+        public double Man()
+        {
+            return area / amount;
+        }
+        /// <summary>
+        /// Доступ к полю площадь 
+        /// </summary>
+        /// <returns>area - площадь</returns>
+        public double totalS()
+        {
+            return area;
+        }
+        /// <summary>
+        /// Доступ к полю кол-во проживающих 
+        /// </summary>
+        /// <returns>amount - кол-во проживающих</returns>
+        public int totalA()
+        {
+            return amount;
+        }
+    }
+    /// <summary>
+    /// Производный класс "Проживающие"
+    /// <author>Korjagin_VU</author> 
+    /// <version>1.9</version> 
+    /// <warning>Данный класс создан в учебных целях</warning> 
+    /// Обычный дочерний класс, который отнаследован от ранее созданного класса aeroflot
+    /// </summary>
+    class Room_resident : Room
+    {
+        private int adult; // взрослые
+        private int child; // дети
+        /// <summary>
+        /// Конструктор класса Room_resident, инициализирующий переменные
+        /// </summary>
+        /// <param name="s">площадь</param>
+        /// <param name="a">общее кол-во проживающих</param>
+        /// <param name="ad">кол-во взрослых</param>
+        /// <param name="ch">кол-во детей</param>
+        public void Init(double s, int a, int ad, int ch)
+        {
+            base.Init(s, a);
+            adult = ad;
+            child = ch;
+        }
+        /// <summary>
+        /// Отображает данные о взрослых и детях на экране
+        /// </summary>
+        public void Display()
+        {
+            base.Display();
+            Console.WriteLine("	Количество взрослых: " + adult + "\n	Количество детей: " + child + "\n");
+        }
+        /// <summary>
+        /// Считает площадь на человека с разделением на детей и взрослых
+        /// </summary>
+        /// <returns></returns>
+        public double Man()
+        {
+            amount = adult + child * 2;
+            return area / amount;
+        }
+        /// <summary>
+        /// Доступ к полю площадь 
+        /// </summary>
+        /// <returns>area - площадь</returns>
+        public double totalS()
+        {
+            return area;
+        }
+        /// <summary>
+        /// Доступ к полю кол-во проживающих с разделением на детей и взрослых
+        /// </summary>
+        /// <returns>amount - кол-во проживающих</returns>
+        /// Формула для вычисления количества проживающих, при условии, что кол-во детей умножается на 2:
+        /// f$ amount = adult + child \times 2 @f$
+        public int totalA()
+        {
+            return adult + child * 2;
+        }
+    }
+    /// <summary>
+    /// Класс "Квартира"
+    /// Содержит информацию о комната, проживающих в них и их площади
+    /// <details>Также информацию об общей площади и средней площади на человека с учетом общей площади</details>
+    /// </summary>
+    internal class Flat
+    {
+        private char[] name;
+        private Room TypeR1 = new Room();
+        private Room_resident TypeR2 = new Room_resident();
+        private double TotalAreaDop = 0;
+        /// <summary>
+        /// Конструктор Flat, принимает параметры типа Room и его производного
+        /// </summary>
+        /// <param name="nf">Номер квартиры</param>
+        /// <param name="s1">площадь первой комнаты</param>
+        /// ![Image](C:\Users\slava\Downloads\room1.jpg)
+        /// <param name="s2">площадь второй комнаты</param>
+        /// ![Image](C:\Users\slava\Downloads\room2.jpg)
+        /// <param name="a1">общ кол-во проживающих 1 комнаты</param>
+        /// <param name="a2">общ кол-во проживающих 2 комнаты</param>
+        /// <param name="ad2">кол-во взрослых 2 комнаты</param>
+        /// <param name="ch2">кол-во детей 2 комнаты</param>
+        /// <param name="TotalAreaDop1">площадь доп помещений</param>
+        public void Init(char[] nf, double s1, double s2, int a1, int a2, int ad2, int ch2, double TotalAreaDop1)
+        {
+            name = nf;
+            TypeR1.Init(s1, a1);
+            TypeR2.Init(s2, a2, ad2, ch2);
+            TotalAreaDop = TotalAreaDop1;
+        }
+        /// <summary>
+        /// Отображает на экране информацию о квартире
+        /// </summary>
+        public void Display()
+        {
+            Console.WriteLine("\nОбозначение квартиры: ");
+            foreach (char ch in name)
+            {
+                Console.Write(ch);
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+            TypeR1.Display();
+            TypeR2.Display();
+            Console.WriteLine("Общая площадь дополнительных помещений (кухня, кладовка): " + TotalAreaDop);
+        }
+        /// <summary>
+        /// Вычисляет среднюю площадь на человека
+        /// </summary>
+        /// <returns>Площадь на человека с учетом площади доп помещений</returns>
+        public double avrArea()
+        {
+            double TotalArea = TypeR1.totalS() + TypeR2.totalS();  // общая площадь
+            int TotalAmount = TypeR1.totalA() + TypeR2.totalA();  // общее количество людей
+            return (TotalArea + TotalAreaDop) / TotalAmount;
+        }
+    }
+}
---
+++ QA_Lab3/Program.cs
@@ -0,0 +1,32 @@
+﻿using QA_Lab3;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QA_Lab3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Room rr1 = new Room();
+            Room_resident rr2 = new Room_resident();
+            Room_resident rr3 = new Room_resident();
+            rr1.Init(10, 1);
+            rr2.Init(15, 4, 3, 1);
+            rr3.Init(10, 3, 3, 0);
+            Console.WriteLine("Средняя площадь на человека без разделения на взрослых и детей: \n" + rr1.Man());
+            Console.WriteLine("Средняя площадь с разделением на детей и взрослых: \n" + rr2.Man() + "\n" + rr3.Man());
+
+            Flat fl = new Flat();
+            char[] nf = "Квартира №100".ToCharArray();
+            fl.Init(nf, 10, 15, 2, 4, 3, 1, 4);
+            fl.Display();
+            Console.WriteLine("\nСредняя площадь на одного человека по 2 комнатам: " + fl.avrArea());
+            Console.Read();
+        }
+    }
+}
+
